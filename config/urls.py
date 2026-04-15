1cfdd7a7ba4262067725043fd0b2398bb3d24c16
ryumijin <mijin1466@naver.com>
2026-04-15 19:47:43 +0900

    CHAPTER 07 실습코드

--- config/urls.py
+++ config/urls.py
@@ -23,11 +23,12 @@ urlpatterns = [
 
 from django.contrib import admin
 from django.urls import path
-from config.views import main, burger_list
+from config.views import main, burger_list, burger_search
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", main),
     path("burgers/", burger_list),
+    path("search/", burger_search),
 ]
 
--- config/views.py
+++ config/views.py
@@ -24,3 +24,17 @@ def burger_list(request):
         "burgers": burgers,
     }
     return render(request, "burger_list.html", context)
+
+def burger_search(request):
+    keyword = request.GET.get("keyword")
+
+    if keyword is not None:
+        burgers = Burger.objects.filter(name__contains=keyword)
+
+    else:
+        burgers = Burger.objects.none()
+
+    context = {
+        "burgers": burgers,
+    }
+    return render(request, "burger_search.html", context)
