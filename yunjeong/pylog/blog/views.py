da7bca593aeda0bbd70d19a202e0a92e13931ee3
dw261 <woduuc099@gmail.com>
2026-04-17 18:53:58 +0900

    3주차 ch11, 12, 13 실습 코드

--- yunjeong/pylog/blog/views.py
+++ yunjeong/pylog/blog/views.py
@@ -9,3 +9,11 @@ def post_list(request):
     }
     # 3번째 인수로 템플릿에 데이터 전달
     return render(request, "post_list.html", context)
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id) # id값이 URL에서 받은 post_id 값인 Post 객체
+    print(post)                         # 가져온 객체를 print 함수로 확인
+    context = {
+        "post": post,
+    }
+    return render(request, "post_detail.html", context)
