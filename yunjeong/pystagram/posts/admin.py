3966cf077cdfac7289d7a862b6fc0b32517f5ec5
dw261 <woduuc099@gmail.com>
2026-05-11 17:34:53 +0900

    chapter 17 실습코드

--- yunjeong/pystagram/posts/admin.py
+++ yunjeong/pystagram/posts/admin.py
@@ -1,3 +1,39 @@
+import admin_thumbnails
 from django.contrib import admin
+from posts.models import Post, PostImage, Comments
 
-# Register your models here.
+class CommentInline(admin.TabularInline):
+    model = Comments
+    extra = 1
+
+@admin_thumbnails.thumbnail("photo")
+class PostImageInline(admin.TabularInline):
+    model = PostImage
+    extra = 1
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "content",
+    ]
+    inlines = [
+        CommentInline,
+        PostImageInline,
+    ]
+
+@admin.register(PostImage)
+class PostImageAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "post",
+        "photo",
+    ]
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "post",
+        "content",
+    ]
--- yunjeong/pystagram/posts/views.py
+++ yunjeong/pystagram/posts/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
+from posts.models import Post
 
 def feeds(request):
     # 요청에 포함된 사용자가 로그인하지 않은 경우
     if not request.user.is_authenticated:
-        # /user/login/ URL로 이동시킴
         return redirect("/users/login/")
-    return render(request, "posts/feeds.html")
+
+    # 모든 글 목록을 템플릿으로 전달
+    posts = Post.objects.all()
+    context = {"posts": posts}
+    return render(request, "posts/feeds.html", context)
